d580cf4f737399344a449206df027234dad2b03e
Akun97 <970357742@qq.com>
2022-09-16 00:00:01 +0800

    2022年9月16日00:00:04

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,3 @@
-/*
- * @Author: Akun97 17759735780@163.com
- * @Date: 2022-06-13 13:38:57
- * @LastEditors: Akun97 17759735780@163.com
- * @LastEditTime: 2022-06-13 15:01:11
- * @FilePath: \uniapp_template\src\server\index.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 const toast = (msg:string):void => {
   uni.showToast({
     title: msg,
